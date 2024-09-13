ca05dde509fb816c779eeef9415f94dc5a75cc3f
Daniel Arandia <daniel.arandia01@usa.edu.co>
2024-09-13 03:42:28 +0000

    create back.py

--- back.py
+++ back.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
-import psycopg2  # Para PostgreSQL, o usa mysql.connector para MySQL
+import psycopg2  # Para PostgreSQL
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # Permitir peticiones del frontend Angular
 
 # Configuración para conectarse a la base de datos en otra instancia
-DB_HOST = 'IP_O_DOMINIO_DE_LA_INSTANCIA_DB'
-DB_NAME = 'nombre_de_base_de_datos'
-DB_USER = 'usuario'
-DB_PASSWORD = 'contraseña'
+DB_HOST = '54.197.24.118'  # IP o dominio de la instancia DB
+DB_NAME = 'usuarios_db'  # Nombre de la base de datos
+DB_USER = 'flask_user'  # Nombre de usuario
+DB_PASSWORD = ''  # Contraseña vacía si no se requiere
 DB_PORT = '5432'  # Puerto por defecto de PostgreSQL
 
 # Ruta para recibir los datos del frontend
@@ -41,3 +41,4 @@ def receive_data():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
